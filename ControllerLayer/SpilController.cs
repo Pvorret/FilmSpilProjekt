0696888d70f3f023113448d39c3aec6667077992
Pvorret <phvorret@gmail.com>
2015-02-19 11:41:12 +0100

    Opret

---
+++ ControllerLayer/SpilController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Film.Database;
+
+namespace ControllerLayer
+{
+    public class SpilController
+    {
+
+        public string GetTest()
+        {
+            return SpilDB.GetTest();
+
+        }
+    }
+}
---
+++ Film/Database/DBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Film.Database
+{
+    static class DBConnection
+    {
+        public static SqlConnection ConnectToDB()
+        {
+            SqlConnection connect = new SqlConnection(
+                "Server=PVLAPTOP\\PHILLIPSSERVER;" +
+                "Database=FilmSpilDB;" +
+                "User Id=Phil;" +
+                "Password=Phil010592"
+                );
+            return connect;
+        }
+    }
+}
